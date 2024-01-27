32d5156d51fb5e958fe07b93995379038559be23
StefaniaGuta <142820560+StefaniaGuta@users.noreply.github.com>
2024-01-27 04:22:21 -0800

    second commit

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const createGallaryItems = galleryItems
+  .map(({ original, preview, description }) =>
+    `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-src="${original}" alt="${description}" />
+      </a>
+    </li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML('beforeend', createGallaryItems);
+
+const galleryHandler = new SimpleLightbox('ul.gallery a', { 
+  captionsData: 'alt',
+  captionDelay: 250
+});
+galleryHandler.on("show.simplelightbox");
